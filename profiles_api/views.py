7eb4b790c8ff0af9d9c8dcc9e263a167d5980ffe
spx <vahid.saeedi7495@gmail.com>
2021-08-27 16:37:11 +0430

    Added Serializer and post, put, path, delet, to the view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of API View features"""
@@ -15,4 +17,26 @@ class HelloAPIView(APIView):
             "Is mapped manually to URLs",
         ]
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Returns a message for the received name data"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Update the entire object"""
+        return Response({"method": "PUT"})
+
+    def patch(self, request, pk=None):
+        """Partially update the object"""
+        return Response({"method": "PATCH"})
+
+    def delete(self, request, pk=None):
+        """Delete object"""
+        return Response({"method": "delete"})
 
